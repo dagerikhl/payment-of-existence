212549589b3bf6ebcf8f20ff533d913cc1da5da7
Dag Erik <dagerikhl@gmail.com>
2017-11-29 21:02:59 +0100

    Make more CSS improvements
    Attempt at making site layout consistent across viewport sizes.

--- src/components/app/Footer.js
+++ src/components/app/Footer.js
@@ -6,13 +6,13 @@ class Footer extends Component {
     render() {
         return (
             <footer className='app-footer'>
-                <span className='year'>2017</span>
-                <span className='made-by'>
+                <h5>2017</h5>
+                <h5>
                     made by&nbsp;
                     <a className='sec-link'
                        target='_blank' rel='noopener noreferrer'
                        href='https://github.com/dagerikhl/'>(dagerikhl)</a>
-                </span>
+                </h5>
             </footer>
         );
     }
